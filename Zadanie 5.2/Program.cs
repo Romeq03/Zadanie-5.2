1ea9867c9cc624ff957fe5712e631ffd13b6a245
Romeq03 <romekbartek18@gmail.com>
2024-02-04 22:23:10 +0100

    first commit

---
+++ Zadanie 5.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Wprowadzenie pierwszego łańcucha tekstowego
+        Console.Write("Podaj pierwszy łańcuch tekstowy: ");
+        string pierwszyLancuch = Console.ReadLine();
+
+        // Wprowadzenie drugiego łańcucha tekstowego
+        Console.Write("Podaj drugi łańcuch tekstowy: ");
+        string drugiLancuch = Console.ReadLine();
+
+        // Konkatenacja łańcuchów
+        string wynik = pierwszyLancuch + drugiLancuch;
+
+        // Wyświetlenie wyniku
+        Console.WriteLine($"Wynik konkatenacji: {wynik}");
+
+        // Zatrzymanie programu do momentu naciśnięcia klawisza Enter
+        Console.ReadLine();
+    }
+}
