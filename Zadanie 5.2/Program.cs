22084369e281504ed4ebee3ba74bd418da47a216
Bartłomiej Romek <148621924+Romeq03@users.noreply.github.com>
2024-02-08 12:01:16 +0100

    Update Program.cs

--- Zadanie 5.2/Program.cs
+++ Zadanie 5.2/Program.cs
@@ -4,21 +4,16 @@ class Program
 {
     static void Main()
     {
-        // Wprowadzenie pierwszego łańcucha tekstowego
         Console.Write("Podaj pierwszy łańcuch tekstowy: ");
         string pierwszyLancuch = Console.ReadLine();
 
-        // Wprowadzenie drugiego łańcucha tekstowego
         Console.Write("Podaj drugi łańcuch tekstowy: ");
         string drugiLancuch = Console.ReadLine();
 
-        // Konkatenacja łańcuchów
         string wynik = pierwszyLancuch + drugiLancuch;
 
-        // Wyświetlenie wyniku
         Console.WriteLine($"Wynik konkatenacji: {wynik}");
 
-        // Zatrzymanie programu do momentu naciśnięcia klawisza Enter
         Console.ReadLine();
     }
 }
